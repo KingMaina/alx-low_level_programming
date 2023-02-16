a18caef10736138b1e13f985e91e683ad40dbdb6
Andrew Maina <drew29799@gmail.com>
2023-02-16 07:08:17 -0800

    Fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/*
+ * main - Entry point
+ *
+ * prints a string to stdout using puts()
+ *
+ * return 0(success)
+ *
+ * */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
