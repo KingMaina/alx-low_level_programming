7472abef695907e8e8a7ab657deea2fb1e855848
Andrew Maina <drew29799@gmail.com>
2023-02-16 07:34:18 -0800

    Added 5-printf.c that prints a string to stdout using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Prints a string to stdout using printf()
+ *
+ * Returns: 0 (sucess)
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is apiece of art,\n");
+	return (0);
+}
