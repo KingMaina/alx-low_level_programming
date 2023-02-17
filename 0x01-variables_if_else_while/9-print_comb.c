6f4d9973480cb071c8ed9fb4282acdb3753a78a3
Andrew Maina <drew29799@gmail.com>
2023-02-17 06:12:20 -0800

    Fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,7 +3,8 @@
 /**
  * main - Entry point
  *
- * Description: Prints all possible combinations of single-digit numbers
+ * Description: Prints all possible combinations of 
+ * single-digit numbers separated by commas.
  * Return: 0 (Success)
  */
 int main(void)
@@ -13,6 +14,12 @@ int main(void)
 	while (number <= 57)
 	{
 		putchar(number);
+		if (number == 57)
+		{
+			break;
+		}
+		putchar(44);
+		putchar(32);
 		number++;
 	}
 	putchar(10);
