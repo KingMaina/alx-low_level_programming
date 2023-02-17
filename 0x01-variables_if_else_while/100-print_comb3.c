2ae8abbf01c96ef9c2242bc2ac9f3175f558eeec
Andrew Maina <drew29799@gmail.com>
2023-02-17 08:06:07 -0800

    Added C code to print all combos of 2 digits
    Prints all the combinations of two 2 digits [0-9] and each combo must be
    different.

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible combinations of two digits using putchar()
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int number = 48;
+	int nextNum;
+
+	for (; number <= 57; number++)
+	{
+		nextNum = number + 1;
+		for (; nextNum <= 57; nextNum++)
+		{
+			putchar(number);
+			putchar(nextNum);
+			if (number == 56 && nextNum == 57)
+			{
+				break;
+			}
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar(10);
+
+	return (0);
+}
