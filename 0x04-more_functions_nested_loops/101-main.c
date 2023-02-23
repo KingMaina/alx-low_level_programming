7c01e3b7f1a26194f92e2017c5de0d95dc7d49b6
Andrew Maina <drew29799@gmail.com>
2023-02-23 15:19:45 -0800

    Fix

---
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - runs the program
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	print_number(1024);
+	printf("\n");
+	print_number(-1024);
+	printf("\n");
+	print_number(0);
+	printf("\n");
+	print_number(124);
+	printf("\n");
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* print_number - prints a number
+* @n: integer
+*
+*/
+void print_number(int n)
+{
+	int divisor = 1;
+	int digit;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	while (n / divisor >= 10)
+		divisor *= 10;
+	while (divisor != 0)
+	{
+		digit = n / divisor;
+		_putchar(digit + '0');
+		n % divisor;
+		divisor %= 10;
+	}
+}
