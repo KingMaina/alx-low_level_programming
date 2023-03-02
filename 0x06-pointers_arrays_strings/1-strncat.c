2d6703886c549141a2f0a73d56b24179f1691841
Andrew Maina <drew29799@gmail.com>
2023-03-02 00:40:38 -0800

    Added C code to concatenate and copy a string
    - *_strncpy copies a string from the source string to the destination string.
    - *_strncat concatenates a source string to the destinaton string.
    - Both functions use a parameter n to limit the number of bytes to copy
    - or concatenate.

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _strncat - concatenates two strings.
+* @dest: The destination string.
+* @src: The source string.
+* @n: The maximum number of bytes to copy from source string.
+
+* Return: A pointer to the destination string.
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	char *destCopy = dest;
+
+	while (*dest)
+		dest++;
+	while (n--)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
+	return (destCopy);
+}
