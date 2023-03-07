baab56ddc01c4e7ae23fda713f2f9288fa59c683
Andrew Maina <drew29799@gmail.com>
2023-03-07 00:08:47 -0800

    Refactored memset to not use n parameter directly

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,11 +10,11 @@
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-	while (n > 0)
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
 	{
-		*s = b;
-		n--;
-		s++;
+		s[i] = b;
 	}
 
 	return (s);
