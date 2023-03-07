74c699f5581306f1cd5844cf5ee1a7257a74d72d
Andrew Maina <drew29799@gmail.com>
2023-03-06 23:50:28 -0800

    Added _putchar.c function declaration

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
+int _putchar(char c);
 void simple_print_buffer(char *buffer, unsigned int n);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
