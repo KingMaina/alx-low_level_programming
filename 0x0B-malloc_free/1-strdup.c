72eb099098680e5645f6b5240cea604f07f01b25
Andrew Maina <drew29799@gmail.com>
2023-03-13 07:36:17 -0700

    Fix

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,6 +2,24 @@
 #include "main.h"
 
 /**
+* _strlen - returns the length of a string.
+* @s: string
+*
+* Return: The length of te string.
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
 * *_strdup - duplicates a string and stores it in the memory heap.
 * @str: The string to duplicate
 *
