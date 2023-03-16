40686630293cf3d3e7a6b340de0591d01ff943a4
Andrew Maina <drew29799@gmail.com>
2023-03-16 15:36:33 -0700

    Add function to create an array of integers within a range

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,6 +6,8 @@ int _putchar(char c);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 int _strlen(char *s);
 void *_calloc(unsigned int nmemb, unsigned int size);
-void simple_print_buffer(char *buffer, unsigned int size);
+/* void simple_print_buffer(char *buffer, unsigned int size);*/
+int *array_range(int min, int max);
+void simple_print_buffer(int *buffer, unsigned int size);
 
 #endif
