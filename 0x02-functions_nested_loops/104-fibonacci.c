27778dd4c6e8830bb909b9db1f67a43f67a954e0
Andrew Maina <drew29799@gmail.com>
2023-03-18 04:19:00 -0700

    Fixed function to handle large fibonacci value with long int

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,20 +11,18 @@ int main(void)
 {
 	int i;
 	int maxCount = 98;
-	unsigned int fib1 = 1;
-	unsigned int fib2 = 2;
-	unsigned int fib3;
+	unsigned long int fib1 = 1;
+	unsigned long int fib2 = 2;
+	unsigned long int fib3;
 
-	printf("%u, %u, ", fib1, fib2);
+	printf("%lu, %lu, ", fib1, fib2);
 	for (i = 0; i < maxCount; i++)
 	{
 		fib3 = fib1 + fib2;
-		printf("%u, ", fib3);
+		printf("%lu, ", fib3);
 		fib1 = fib2;
-		fib2 = fib3 >> 1;
+		fib2 = fib3;
 	}
-	fib3 = fib1 + fib2;
-	printf("%u\n", fib3);
 
 	return (0);
 }
