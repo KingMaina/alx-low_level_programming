461d5d7ba25ec554b9e5384a7a795d4cb2e5cead
Andrew Maina <drew29799@gmail.com>
2023-03-21 04:02:58 -0700

    Fix logic for functions that manipulate dog struct
    - Added parenthesis on ABS parameter to avoid ambiguous logic in macro function
    - Removed unnecessary import 'main.h' in init_dog file
    - Fixed logic for allocating memory of a dog struct in new_dog
    - Fixed logic for deallocating memory of a dog struct in free_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stddef.h>
+#include <string.h>
 #include "dog.h"
 
 /**
@@ -14,34 +15,35 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dogPtr;
 	char *dogName, *dogOwner;
-	int i;
+	int i, dogNameLength, dogOwnerLength;
 
+	dogNameLength = strlen(name) + 1;
+	dogOwnerLength = strlen(owner) + 1;
 	dogPtr = malloc(sizeof(dog_t));
 	if (dogPtr == NULL)
 		return (NULL);
-	dogName = malloc(sizeof(dogPtr->name));
+	dogName = malloc(dogNameLength);
 	if (dogName == NULL)
 	{
 		free(dogPtr);
 		return (NULL);
 	}
-	dogOwner = malloc(sizeof(dogPtr->owner));
+	dogOwner = malloc(dogOwnerLength);
 	if (dogOwner == NULL)
 	{
 		free(dogName);
 		free(dogPtr);
 		return (NULL);
 	}
-	for (i = 0; name[i]; i++)
+	for (i = 0; i < dogNameLength - 1; i++)
 		dogName[i] = name[i];
-	for (i = 0; owner[i]; i++)
-		dogOwner[i] = name[i];
+	dogName[i] = '\0';
+	for (i = 0; i < dogOwnerLength - 1; i++)
+		dogOwner[i] = owner[i];
+	dogOwner[i] = '\0';
 	dogPtr->name = dogName;
 	dogPtr->age = age;
 	dogPtr->owner = dogOwner;
-	free(dogOwner);
-	free(dogName);
 
 	return (dogPtr);
 }
-
