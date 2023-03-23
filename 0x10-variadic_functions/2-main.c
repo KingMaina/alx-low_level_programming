5f915f2d47694a03231be91f1e3244b4011b91af
Andrew Maina <drew29799@gmail.com>
2023-03-23 15:03:58 -0700

    Add variadic functions
    - Added library dependency in header file
    - Added print_numbers to print numbers passed into a variadic function
    - Added print_strings to print strings passed into a variadic function

---
+++ 0x10-variadic_functions/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+* main - Entry point of app
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	print_strings(", ", 2, "Andrew", "Maina");
+	return (0);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,7 @@
 #ifndef VARIADIC_FUNC
 #define VARIADIC_FUNC
 
+#include <stdio.h>
 #include <stdarg.h>
 
 int _putchar(char c);
