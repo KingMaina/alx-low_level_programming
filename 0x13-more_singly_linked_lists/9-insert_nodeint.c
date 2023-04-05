6644bcbb1aece87bc41bcb99cbd24d3a0543fba9
Andrew Maina <drew29799@gmail.com>
2023-04-05 15:10:34 -0700

    Fixed function edge case
    - `insert_nodeint` now handles the case of adding to the end of the linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -59,6 +59,30 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 }
 
 /**
+* listint_len - Counts the number of nodes
+* in a listint_t list
+* @h: pointer to the head node
+*
+* Return: The number of nodes
+*/
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *listPtr = h;
+	size_t numberOfNodes = 0;
+
+	if (h == NULL)
+		return (0);
+	while (listPtr->next != NULL)
+	{
+		numberOfNodes++;
+		listPtr = listPtr->next;
+	}
+	numberOfNodes++;
+
+	return (numberOfNodes);
+}
+
+/**
 * insert_nodeint_at_index - insert a node at the nth index
 * of a listint_t linked list
 * @head: pointer to a pointer to the head node
@@ -69,26 +93,30 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *index_node, *traverse;
+	listint_t *new_node, *current_node, *traverse;
 
 	if (head == NULL)
 		return (NULL);
-	traverse = *head;
 	if (*head == NULL || idx == 0)
 	{
 		new_node = add_nodeint(head, n);
 		return (new_node);
 	}
-	index_node = get_nodeint_at_index(*head, idx);
-	if (index_node == NULL)
+	if (idx == listint_len(*head))
+		return(add_nodeint_end(head, n));
+	current_node = get_nodeint_at_index(*head, idx);
+	if (current_node == NULL)
 		return (NULL);
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	while (traverse->next != index_node)
+	traverse = *head;
+	while (traverse->next != current_node)
+	{
 		traverse = traverse->next;
-	new_node->next = index_node;
+	}
+	new_node->next = current_node;
 	traverse->next = new_node;
 
 	return (new_node);
