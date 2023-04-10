5ea8bb0d3a77eb06a12e8dcd3b4f4be8a91da7df
Andrew Maina <drew29799@gmail.com>
2023-04-10 15:51:47 -0700

    Add function to create a file
    create_file creates a file and assigns read and write permissions to the owner.
    - Added _strlen to calculate length of a string

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,5 +9,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_to_text(const char *filename, char *text_content);
+size_t _strlen(const char *);
 
 #endif /* #ifndef FILE_IO_H */
