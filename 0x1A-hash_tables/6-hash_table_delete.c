73a7e6ab046bdd33989f9e1f837e33d92ad93966
Andrew <drew29799@gmail.com>
2023-09-14 02:46:39 +0300

    Fix: betty styles

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -8,23 +8,23 @@
 */
 void hash_table_delete(hash_table_t *ht)
 {
-    hash_node_t *curr_ptr = NULL, *next_ptr = NULL;
-    unsigned long int index;
+	hash_node_t *curr_ptr = NULL, *next_ptr = NULL;
+	unsigned long int index;
 
-    if (!ht || !(ht->array))
-        return;
-    for (index = 0; index < ht->size; index++)
-    {
-        curr_ptr = ht->array[index];
-        while (curr_ptr)
-        {
-            next_ptr = curr_ptr->next;
-            free(curr_ptr->key);
-            free(curr_ptr->value);
-            free(curr_ptr);
-            curr_ptr = next_ptr;
-        }
-    }
-    free(ht->array);
-    free(ht);
+	if (!ht || !(ht->array))
+		return;
+	for (index = 0; index < ht->size; index++)
+	{
+		curr_ptr = ht->array[index];
+		while (curr_ptr)
+		{
+			next_ptr = curr_ptr->next;
+			free(curr_ptr->key);
+			free(curr_ptr->value);
+			free(curr_ptr);
+			curr_ptr = next_ptr;
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
