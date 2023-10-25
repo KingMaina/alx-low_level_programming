a08f5e9dfe7bd84df901aff088a009fdef246aac
Andrew <drew29799@gmail.com>
2023-10-26 01:03:45 +0300

    Add: 5-island_perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Island perimeter module"""
+
+
+def island_perimeter(grid):
+    """Calculate the perimeter of an island
+        Args:
+            grid (list): List of list of integers
+    """
+    pass
