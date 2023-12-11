dac187305b76a4203571639dc05cd905b60362f1
Andrew <drew29799@gmail.com>
2023-12-12 01:11:49 +0300

    Betty styles

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,8 +1,18 @@
 #include "search_algos.h"
 
+/**
+* linear_search - Searches an array using linear search algorithm
+* @array: Array of integers
+* @size: Size of the array
+* @value: Value to search for in the array
+*
+* Return: The index of the value that was found in the array,
+* otherwise -1 if value is not foiund or array is NULL
+*/
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
+
 	if (array == NULL)
 		return (-1);
 	for (i = 0; i < size; i++)
